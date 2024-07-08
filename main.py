eb247ee240c72387d848f4373442cb7a69990ba5
SungminChoi <csm5099@gmail.com>
2024-07-08 14:29:22 +0900

    피드파서를 통해 rss 정보 파싱

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import feedparser
 
 
-def request():
+def parser_rss():
     url = "http://www.dailysecu.com/rss/S1N2.xml"
     feeds = feedparser.parse(url)
-    print(feeds)
+
+    if feeds.entries:
+        for entry in feeds.entries:
+            print(entry.title)
+            print(entry.description)
+            print(entry.author)
+            print(entry.published)
+            print(entry.link)
+    else:
+        print("none")
 
 
-request()
+parser_rss()
